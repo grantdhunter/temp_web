15aec00da0fa52352aa7dd64533d80b74f8170a2
grantdhunter <gr@nt-hunter.ca>
2014-09-05 00:04:39 -0600

    about content and format

--- public/system/controllers/projectsCtrl.js
+++ public/system/controllers/projectsCtrl.js
@@ -1,7 +1,13 @@
-function projectsCtrl($scope, $routeParams){
-    
-    $scope.projects = [{title:'SilentZone',
-                       link:'http://silentzone.ca',
-                       description:'Android app that automatically changes ringtone settings bassed on location. Go to <a href="http://silentzone.ca/"> silentzone.ca</a> for the full scoop, or if you want to download it, head to the <a href="https://play.google.com/store/apps/details?id=ca.silentzone.silentzone">google play store</a>.',}];
+function projectsCtrl($scope, $routeParams) {
+
+    $scope.projects = [{
+        title: 'SilentZone',
+        link: 'http://silentzone.ca',
+        description: 'Android app that automatically changes ringtone settings based on location. Go to <a href="http://silentzone.ca/"> silentzone.ca</a> for the full scoop, or if you want to download it, head to the <a href="https://play.google.com/store/apps/details?id=ca.silentzone.silentzone">google play store</a>.'
+    }, {
+        title: 'Webpage Under Contruction',
+        link: 'http://undercontruction.grant-hunter.ca',
+        description: 'A silly little static page served up by an express.js server. It uses bootstrap i386 to add some MS-DOS retro flavoring. My favorite part is the page loading animation.'
+    }];
 
 }
